245602af8e1beb548d20312147984aa926d8c1c9
bekjonbegmatov <behruzbegmatov28@@gmail.com>
2024-02-07 20:06:03 +0800

    Adding magistrature parcer

--- parcer/get_groups_list.py
+++ parcer/get_groups_list.py
@@ -26,4 +26,24 @@ def get_list():
     with open('groups_list.json', 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
 
-get_list()
+def get_magis():
+    """ This function for parcing magistrs and kolejs """
+    i = 1
+    data = {"1" : {}, "2" : {}, "3" : {}, "5" : {}, "6" : {}}
+    while i != 108 :#214 :
+        res = requests.get(f'https://portal.esstu.ru/spezialitet/{i}.htm').text
+        soup = BeautifulSoup(res, 'lxml')
+        if i <= 17 : data["1"][soup.find_all('font')[1].get_text().encode("latin-1").decode('cp1251')[1:]] = f'https://portal.esstu.ru/spezialitet/{i}.htm'
+        elif i > 17 and i <= 35 : data['2'][soup.find_all('font')[1].get_text().encode("latin-1").decode('cp1251')[1:]] = f'https://portal.esstu.ru/spezialitet/{i}.htm'
+        elif i > 35 and i <= 56 : data['3'][soup.find_all('font')[1].get_text().encode("latin-1").decode('cp1251')[1:]] = f'https://portal.esstu.ru/spezialitet/{i}.htm'
+        elif i > 56 and i <= 81 : data['5'][soup.find_all('font')[1].get_text().encode("latin-1").decode('cp1251')[1:]] = f'https://portal.esstu.ru/spezialitet/{i}.htm'
+        elif i > 82 and i <= 107 : data['6'][soup.find_all('font')[1].get_text().encode("latin-1").decode('cp1251')[1:]] = f'https://portal.esstu.ru/spezialitet/{i}.htm'
+        
+        # data[soup.find_all('font')[1].get_text().encode("latin-1").decode('cp1251')[1:]] = i
+        print(f'[ INFO ] {soup.find_all('font')[1].get_text().encode("latin-1").decode('cp1251')} : {f'https://portal.esstu.ru/spezialitet/{i}.htm'} , SAVED !')
+        i+=1
+    
+    with open('magistr.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+
+get_magis()
